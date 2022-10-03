ab3f11c6bc128b9247046f5948f9686300e82733
JlIzIg <113330694+JlIzIg@users.noreply.github.com>
2022-10-03 12:44:42 +0300

    Create InfoMarket.java

---
+++ Lab4/src/InfoMarket.java
@@ -0,0 +1,64 @@
+public class InfoMarket {
+    private Market[] market;
+    private String salesman;
+
+
+    public Market[] getMarket() {
+        return market;
+    }
+
+    public void setMarket(Market[] market) {
+        this.market = market;
+    }
+
+    public String getSalesman() {
+        return salesman;
+    }
+
+    public void setSalesman(String salesman) {
+        this.salesman = salesman;
+    }
+
+    public InfoMarket() {
+    }
+
+    public InfoMarket(Market[] market, String salesman) {
+        this.market = market;
+        this.salesman = salesman;
+    }
+
+    public static int[] getInfoNumberOfSellingProducts(Market[] markets, String salesman) {
+        int N = 0;
+        int quantity = 0;
+        for (int i = 0; i < markets.length; i++) {
+            if (markets[i].getSalesman().equals(salesman)) {
+                quantity += markets[i].getQuantity();
+                N++;
+            }
+        }
+        int[] arr = {quantity, N};
+        return arr;
+    }
+
+    public static Market[] getArraySoldProducts(Market[] markets, String salesman) {
+        int[] arr = getInfoNumberOfSellingProducts(markets, salesman);
+        int N = arr[1];
+        Market[] markets1 = new Market[N];
+        for (int i = 0; i < markets.length; i++)
+            for (int j = 0; j < N; j++) {
+                if (markets[i].getSalesman().equals(salesman)) {
+                    markets1[j] = new Market(markets[i].getSalesman(), markets[i].getName_product(), markets[i].getQuantity(), markets[i].getPrice(), markets[i].getDate_seller());
+                }
+            }
+        return markets1;
+    }
+
+    public static Market getHighestPrice(Market[] markets) {
+        int max_index = 0;
+        for (int i = 0; i < markets.length - 1; i++) {
+            if (markets[max_index].getPrice() < markets[i + 1].getPrice()) max_index = i + 1;
+        }
+        return markets[max_index];
+    }
+}
+
