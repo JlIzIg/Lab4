3f93d031eeb7a856923b7ca85d922f637ce682d8
JlIzIg <113330694+JlIzIg@users.noreply.github.com>
2022-10-03 16:56:54 +0300

    Update WorkersInfo.java

--- Lab4/src/WorkersInfo.java
+++ Lab4/src/WorkersInfo.java
@@ -5,44 +5,21 @@ import java.util.Arrays;
 public class WorkersInfo {
 
     private Worker[] workers;
-    private int N;
-
-
-    public Worker[] getWorkers() {
-        return workers;
-    }
-
-    public void setWorkers(Worker[] workers) {
-        this.workers = workers;
-    }
-
-    public int getN() {
-        return N;
-    }
-
-    public void setN(int n) {
-        N = n;
-    }
-
-
     public WorkersInfo() {
 
     }
-
-    public WorkersInfo(Worker[] workers, int n) {
+    public WorkersInfo(Worker[] workers) {
         this.workers = workers;
-        N = n;
-    }
 
+    }
     /**
      * метод возвращающий индекс работника с наименьшим возрастом
      **/
-    public static int getMinAge(Worker[] workers, int N) {
+    public int getMinAge(int N) {
         int[] age = new int[N];
         for (int i = 0; i < N; i++) {
             age[i] = Period.between(workers[i].getBirthday(), LocalDate.now()).getYears();
         }
-        System.out.println(Arrays.toString(age));
         int min_index = 0;
         for (int i = 0; i < workers.length - 1; i++) {
             if (age[min_index] > age[i + 1]) min_index = (i + 1);
@@ -51,10 +28,10 @@ public class WorkersInfo {
     }
 
     /**
-     * метод возвращающий строку информации о сотруднике из массива сотрудников и id
+     * метод возвращающий строку информации о сотруднике по id
      **/
-    public static String getInfo(Worker[] work, int id) {
-        return "Worker is " + work[id].getName() + " " + work[id].getSurname() + " " + work[id].getFather_name() + ". Post is " + work[id].getPost() + ". Salaries is " + work[id].getSalaries() + ". Date of birthday is " + work[id].getBirthday();
+    public String getInfo(int id) {
+        return "Worker is " + workers[id].getName() + " " + workers[id].getSurname() + " " + workers[id].getFather_name() + ". Post is " + workers[id].getPost() + ". Salaries is " + workers[id].getSalaries() + ". Date of birthday is " + workers[id].getBirthday();
     }
 
 }
